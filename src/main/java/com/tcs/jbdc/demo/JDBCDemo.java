9e18641f545b6e9ed335ab66ed7e4ed1de944209
tcsswapnika <tcsswapnika@gmail.com>
2021-08-16 08:48:01 +0000

    jdbc commands

--- src/main/java/com/tcs/jbdc/demo/JDBCDemo.java
+++ src/main/java/com/tcs/jbdc/demo/JDBCDemo.java
@@ -5,34 +5,51 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JDBCDemo {
 	public static void main(String[] args) {
-		String DB_URL = "jdbc:mysql://localhost/practice";
+		String DB_URL = "jdbc:mysql://localhost:3306/practice";
 		String DB_USER = "root";
 		String DB_PASSWORD = "Nuvelabs123$";
-		
-		try(Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-				Statement statement = connection.createStatement();){
-	//			insert();
-				retrieve(statement);
-			
+
+		try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+				Statement statement = connection.createStatement();) {
+			// insert(statement);
+			// retrieve(statement);
+			update(statement);
+			// List<String> regions = retrieveWithCondition(statement, "A");
+
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
-				
+
 	}
-	private static void retrieve(Statement statement) throws SQLException{
+
+	private static List<String> retrieveWithCondition(Statement statement, String string) {
+		return null;
+	}
+
+	private static void retrieve(Statement statement) throws SQLException {
 		ResultSet resultSet = statement.executeQuery("SELECT * from regions");
+		List<String> regions = new ArrayList<String>();
 		while (resultSet.next()) {
 			System.out.println(resultSet.getInt(1));
 			System.out.println(resultSet.getNString("REGION_NAME"));
+			regions.add(resultSet.getNString("REGION_NAME"));
 		}
 	}
+
 	private static void insert(Statement statement) throws SQLException {
+
 		statement.execute("INSERT INTO REGIONS VALUES(5, 'asia')");
-		
+
+	}
+
+	private static void update(Statement statement) throws SQLException {
+		statement.executeUpdate("UPDATE regions set region_name = 'Antartica' where region_id = 5");
 	}
 
 }
